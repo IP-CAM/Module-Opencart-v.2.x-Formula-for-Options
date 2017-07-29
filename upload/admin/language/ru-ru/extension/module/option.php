792cae196efb3db5ccb26b935c979ef577864739
Pavel Garaschenko <bingogg@i.ua>
2017-07-29 11:58:11 +0300

    3 Step Comments model and controller

---
+++ upload/admin/language/ru-ru/extension/module/option.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['heading_title']       		= 'Формула для Опций';
+
+// Text
+$_['text_success']          	= 'Формулы Опций изменены!';
+$_['text_list']          		= 'Список опций';
+$_['text_add']          		= 'Добавление опции';
+$_['text_edit']          		= 'Редактирование опции';
+$_['text_choose']          		= 'Выбор';
+$_['text_select']          		= 'Список';
+$_['text_radio']          		= 'Переключатель';
+$_['text_checkbox']          	= 'Флажок';
+$_['text_input']          		= 'Поле ввода';
+$_['text_text']          		= 'Текст';
+$_['text_textarea']          	= 'Текстовая область';
+$_['text_file']          		= 'Файл';
+$_['text_date']          		= 'Дата';
+$_['text_datetime']          	= 'Дата и время';
+$_['text_time']          		= 'Время';
+
+// Column
+$_['column_name']          		= 'Опция';
+$_['column_sort_order']         = 'Порядок сортировки';
+$_['column_action']          	= 'Действие';
+
+// Entry
+$_['entry_option_value']        = 'Название опции:';
+$_['entry_image']          		= 'Добавление значенний:';
+$_['entry_sort_order']          = 'Формула:';
+
+//Entry constant
+$_['entry_formula_option_value_const']        = 'Название значения:';
+$_['entry_formula_const']          		      = 'Выбор типа значения';
+$_['entry_formula_value']                     = 'Ввод значения:';
+
+// Error
+$_['error_permission']          = 'У вас нет прав для изменения опций!';
+$_['error_name']          		= 'Имя опции должно быть от 1 до 128 символов!';
+$_['error_type']          		= 'Не указано значение опции!';
+$_['error_option_value']        = 'Значение опции должно быть от 1 до 128 символов!';
+$_['error_product']          	= 'Эта опция не может быть удалена, поскольку она присвоена %s товарам!';
